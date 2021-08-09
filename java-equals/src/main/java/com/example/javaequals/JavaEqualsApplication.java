4d68280d339ddc0656d7f639b193b141e3942496
Hyuck Kwon <hkwon@pyrokorea.com>
2021-08-09 23:15:45 +0900

    first commit

---
+++ java-equals/src/main/java/com/example/javaequals/JavaEqualsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaequals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEqualsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEqualsApplication.class, args);
+    }
+
+}
---
+++ java-equals/src/test/java/com/example/javaequals/JavaEqualsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaequals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEqualsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
